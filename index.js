b6dd753bd4f72b1a77129d771a5fdd7bd5b85b44
AhmadAbd9843 <145836866+AhmadAbd9843@users.noreply.github.com>
2025-01-15 05:30:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videos'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
+//UPDATED
